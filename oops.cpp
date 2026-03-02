ef79f76029dac48ea90221fcb68f2b3120f231e8
Hasib303 <hasib303bd@gmail.com>
2026-03-02 23:57:53 +0600

    Refactor Teacher class: add parameterized constructor and update getInfo method

--- oops.cpp
+++ oops.cpp
@@ -14,9 +14,24 @@ private:
     double salary;
 
 public:
+    //non-parameterize constructure
+    // Teacher(){
+    //     dept = "computer science";
+    //     cout << "Hi, I'm the teacher" << endl;
+    // }
+
+    //parameterize constructure
+    Teacher(string n, string d, string s, double sal){
+        name = n; 
+        dept = d;
+        subject = s;
+        salary = sal;
+
+    }
     //properties / attributes
     string name;
     string dept;
+    string subject;
 
     //methods / functions
     void changeDept(string newDept){
@@ -32,19 +47,32 @@ public:
     double getSalary(){
         return salary;
     }
+
+    void getInfo(){
+        cout << "name : " << name << endl;
+        cout << "subject : " << subject << endl;
+    }
+
 };
 
 int main()
 {
-    Teacher t1;
-    t1.name = "Hasib";
-    t1.dept = "CSE";
-    // t1.salary = 50000;
+    // Teacher t1; //automatically call the default constructor
+    // t1.name = "Hasib";
+    // // t1.dept = "CSE";
+    // // t1.salary = 50000;
 
-    t1.setSalary(60000);
+    // t1.setSalary(60000);
 
-    cout << "Name: " << t1.name << endl;
-    cout << "Salary: " << t1.getSalary() << endl;
+    // cout << "Name: " << t1.name << endl;
+    // cout << "Salary: " << t1.getSalary() << endl;
+    // cout << t1.dept << endl;
+
+    Teacher t1 ("Hasib", "CSE", "C++", 50000);
+    t1.getInfo();
+
+
     
 return 0;
 }
+
